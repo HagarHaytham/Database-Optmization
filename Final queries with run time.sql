85e56509def010fe6e5e299b08ac8594cb65bf77
hagarhaytham <hagar.bahy97@eng-st.cu.edu.eg>
2019-12-25 00:09:35 +0200

    adding running times of stored procedures

--- Final queries with run time.sql
+++ Final queries with run time.sql
@@ -1,30 +1,24 @@
-DELIMITER  //
-CREATE PROCEDURE  `get_aided_students`()
-BEGIN
+-- Final queries
+--q1
 SELECT FirstName ,SSN, NumberOfPieces FROM student , help , helpreceived
 WHERE ( student.SSN = helpreceived.StudntID AND
         help.DateRecieved = helpreceived.HelpDate AND
         help.Kind = helpreceived.Helpkind );
 
-END//
-DELIMITER ;
-
-DELIMITER $$
-CREATE PROCEDURE  `get_class_attendance_count`()
-BEGIN
+--q2
 SELECT student.SSN,COUNT(*) from studentlesson,class,student,lesson
 WHERE (studentlesson.Attended = 1 and(class.Code= 1 or class.Code =2) and student.SSN<100 and studentlesson.StudentID = student.SSN
       and lesson.ClassID = class.Code)
 GROUP BY student.SSN;
-END$$
-DELIMITER ;
 
-DELIMITER //
-CREATE PROCEDURE  `get_students_volunteer_info`()
-BEGIN
-
+--q3
 SELECT DISTINCT student.SSN ,student.FirstName,student.LastName, volunteer.FirstName from student,studentclass,class,volunteer
 WHERE (student.SSN = studentclass.StudentID and studentclass.ClassID = class.Code and class.Teacher = volunteer.SSN)
 order by student.FirstName;
-END//
-DELIMITER ;
+
+
+
+--       before      stored procedures      index tuning
+--q1     0.0110       0.4951
+--q2     0.0073       0.0055
+--q3     0.7388       exceeded 300 s
