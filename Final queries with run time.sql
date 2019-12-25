94c26b60097253aaef0b8d9877d416e15d3155fb
hagarhaytham <hagar.bahy97@eng-st.cu.edu.eg>
2019-12-25 14:02:08 +0200

    query trees added

--- Final queries with run time.sql
+++ Final queries with run time.sql
@@ -6,10 +6,6 @@ WHERE ( student.SSN = helpreceived.StudntID AND
         help.Kind = helpreceived.Helpkind );
 
 --q2
--- SELECT student.SSN,COUNT(*) from studentlesson,class,student,lesson
--- WHERE (studentlesson.Attended = 1 and(class.Code= 1 or class.Code =2) and student.SSN<100 and studentlesson.StudentID = student.SSN
---       and lesson.ClassID = class.Code)
--- GROUP BY student.SSN;
 SELECT student.SSN,COUNT(*) from studentlesson,class,student,lesson
 WHERE (studentlesson.Attended = 1 and(class.Code= 1 or class.Code =2) and student.SSN<100 and studentlesson.StudentID = student.SSN
       and lesson.ClassID = class.Code and lesson.ClassID = studentlesson.ClassID and lesson.LessonDate = studentlesson.LessonDate)
@@ -23,9 +19,9 @@ order by student.FirstName;
 
 
 --       before      stored procedures      index tuning      query_optimization
---q1     0.0110       0.4923                  0.0023            0.0164
---q2     0.0034       0.0010                  0.0031            
---q3     0.7388       exceeded 300 s          0.7121  
+--q1     0.0110       0.4923                  0.0023            0.0099
+--q2     0.0108       0.0010                  0.0031            0.0048
+--q3     0.7388       exceeded 300 s          0.7121            0.7350 
 
 
 -- queries after optmization 
@@ -42,8 +38,37 @@ FROM ( (
          as STDNAME )
           WHERE STDNAME.SSN=STDHELP.StudntID
 
+--q2
+SELECT SSN,COUNT(*) FROM
+(
+    (SELECT * FROM
+        ((SELECT SSN FROM student WHERE SSN<100) as S) JOIN
+            ((SELECT * FROM studentlesson WHERE Attended = 1) AS SL) on
+                S.SSN = SL.StudentID
+    ) AS lefttable
+),
+ (
+    (SELECT * From lesson JOIN (
+        ((SELECT Code FROM class) AS C)
+    ) ON lesson.ClassID = C.Code) AS righttable
+) 
+Where lefttable.ClassID = righttable.ClassID and lefttable.LessonDate = righttable.LessonDate
+GROUP BY lefttable.SSN;
+
+
+
+--q3
+SELECT DISTINCT T.SSN ,T.FirstName,T.LastName, CV.FirstName 
+FROM studentclass, 
+( (SELECT SSN, FirstName, LastName FROM student) AS T ), 
+( (SELECT FirstName, Code FROM ((SELECT Code, Teacher from class) AS C) 
+JOIN ((SELECT FirstName,SSN FROM volunteer) AS V) ON C.Teacher = V.SSN) AS CV ) 
+WHERE T.SSN = studentclass.StudentID and studentclass.ClassID = CV.Code 
+order by T.FirstName
+
 
 -- q1:
+-- index student.ssn
 -- index added helpreceived.StudntID ->0.0097 
 -- then index added on help.kind -> time 0.0023
 
